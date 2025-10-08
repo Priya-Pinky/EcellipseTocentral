e3b7501f3ed48975d12530d304a97aae5539ee0c
Priya-Pinky <chepuripriyanka1110@gmail.com>
2025-10-08 13:57:31 +0530

    push the files

---
+++ GitBrowser.java
@@ -0,0 +1,11 @@
+package FrameWorkPratice.FrameWorkPracki;
+
+public class GitBrowser {
+
+	public static void main(String[] args) 
+	{
+WebDriver driver= new ChromeDriver();
+driver.get("");
+	}
+
+}
